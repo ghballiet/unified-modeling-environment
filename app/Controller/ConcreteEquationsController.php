8cbca62426cae2de11e953457cd90426121dee8b
Glen Hunt <glenrhunt@gmail.com>
2012-02-15 10:32:35 -0800

    updated equation controller to allow deletion

--- app/Controller/ConcreteEquationsController.php
+++ app/Controller/ConcreteEquationsController.php
@@ -8,5 +8,12 @@ class ConcreteEquationsController extends AppController {
       $this->redirect(array('controller'=>'unified_models', 'action'=>'view', $this->request->data['ConcreteEquation']['model_id']));
     }
   }
+
+  public function delete($id = null, $model = null) {
+    if($this->ConcreteEquation->delete($id)) {
+      $this->Session->setFlash('Equation succesfully deleted.');
+      $this->redirect(array('controller'=>'unified_models', 'action'=>'view', $model));
+    }
+  }
 }
 ?>
